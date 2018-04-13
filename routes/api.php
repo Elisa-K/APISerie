48ec74e90ac92839fc3eda02f2a6a37a4f9e5952
JulienMounianman <julienmounianman@gmail.com>
2018-04-13 11:53:56 +0200

    Routes fix

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(array(
+    'prefix' => 'v1',
+    'middleware' => 'api'
+),function ()
+{
+    Route::apiResources([
+        'series' => 'SerieController',
+        'seasons' => 'SeasonController',
+        'episodes' => 'EpisodeController'
+    ]);
 });
